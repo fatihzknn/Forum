405d11d2f61173c4872021aeabe9cb10af9ca21b
Fatih Han Özkan <fatihzknn@hotmail.com>
2022-11-03 13:45:37 +0300

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { ArticlesComponent } from './components/articles/articles.component';
 import { LeftbarComponent } from './components/leftbar/leftbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticlesComponent,
     LeftbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
